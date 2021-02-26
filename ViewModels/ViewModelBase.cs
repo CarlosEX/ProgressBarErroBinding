038f2c6369ce73dbd0f6f6cdae66d2d7ba6aea3b
Carlos Antonio <carlos22martinssilva@gmail.com>
2021-02-26 08:10:38 -0300

    commit initial

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace BindingProgressBarError.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
